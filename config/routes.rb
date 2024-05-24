b2f4074ebb65f155af902716a2c46a9873fe321e
SurendraBirla <birlasurendra0805@gmail.com>
2024-05-24 13:41:05 +0530

    Added Chat related features

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'rooms/index'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms do
+    resources :messages
+  end
+  resources :users
+  root 'rooms#index'
+
+  # root to: 'signin'
+end
